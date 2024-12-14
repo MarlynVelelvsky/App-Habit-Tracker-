8fa60a87e31a02342f9722f6b600bc84e65c2bad
MarlynVelelvsky <marin.ka.15162@gmail.com>
2024-12-14 17:13:33 +0100

    commit 2

--- app/src/main/java/com/example/habittracker/MainActivity.kt
+++ app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.habittracker.ui.theme.HabitTrackerTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +26,35 @@ class MainActivity : ComponentActivity() {
         setContent {
             HabitTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    Greeting(modifier = Modifier.padding(innerPadding))}
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    val textState = remember { mutableStateOf("Hello Android!") }
+
+    Column(modifier = modifier.padding(16.dp)){
+        Text(
+            text = textState.value,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Button(
+            onClick = {
+                textState.value = "Habit Added!"
+            }
+        ) {
+            Text(text = "Add Habit")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HabitTrackerTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
